9e50e3def30aa4883bde0bf3789af43354bc991c
Athanasios Raptodimos <thanosra@gmail.com>
2018-04-01 11:59:54 +0300

    Clean up

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <h1>Temporary Title</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
