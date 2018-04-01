dd76c2f7a299e8c58296e8d3e1a6f8b1ef706680
Athanasios Raptodimos <thanosra@gmail.com>
2018-04-01 12:25:44 +0300

    Add Footer component

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,26 @@
     <div class="container view">
       <router-view/>
     </div>
+
+    <the-footer></the-footer>
   </div>
 </template>
 
 <script>
 import TheHero from '@/components/TheHero.vue'
+import TheFooter from '@/components/TheFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    TheHero
+    TheHero,
+    TheFooter
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  min-height: 350px;
+}
 </style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+ <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Customers Management</strong> by <a href="https://www.kalodiodev.eu">KalodioDev</a>.
+          </p>
+        </div>
+      </div>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
