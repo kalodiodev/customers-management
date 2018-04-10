f848c952eccc64c615810e03e3e1202508636408
Athanasios Raptodimos <thanosra@gmail.com>
2018-04-10 06:41:27 +0300

    Add page not found

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import Router from 'vue-router'
 import PageHome from '@/views/PageHome'
 import PageCustomers from '@/views/PageCustomers'
 import PageAbout from '@/views/PageAbout'
+import PageNotFound from '@/views/PageNotFound'
 
 Vue.use(Router)
 
 const routes = [
   { path: '/', name: 'PageHome', component: PageHome },
   { path: '/customers', name: 'PageCustomers', component: PageCustomers },
-  { path: '/about', name: 'PageAbout', component: PageAbout }
+  { path: '/about', name: 'PageAbout', component: PageAbout },
+  { path: '*', name: 'PageNotFound', component: PageNotFound }
 ]
 
 export default new Router({mode: 'history', routes})
