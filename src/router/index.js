02d62ead9fbfd14c78c717920ec20e345a3aeb89
Athanasios Raptodimos <thanosra@gmail.com>
2018-04-10 06:35:17 +0300

    Add customers page

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import PageHome from '@/views/PageHome'
+import PageCustomers from '@/views/PageCustomers'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'PageHome',
-      component: PageHome
-    }
-  ]
-})
+const routes = [
+  { path: '/', name: 'PageHome', component: PageHome },
+  { path: '/customers', name: 'PageCustomers', component: PageCustomers }
+]
+
+export default new Router({mode: 'history', routes})
---
+++ src/views/PageCustomers.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Customers</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
