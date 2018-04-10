2115bd3de851b701760880d9f78b42ce2f2e27a2
Athanasios Raptodimos <thanosra@gmail.com>
2018-04-10 06:38:23 +0300

    Add about page

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import PageHome from '@/views/PageHome'
 import PageCustomers from '@/views/PageCustomers'
+import PageAbout from '@/views/PageAbout'
 
 Vue.use(Router)
 
 const routes = [
   { path: '/', name: 'PageHome', component: PageHome },
-  { path: '/customers', name: 'PageCustomers', component: PageCustomers }
+  { path: '/customers', name: 'PageCustomers', component: PageCustomers },
+  { path: '/about', name: 'PageAbout', component: PageAbout }
 ]
 
 export default new Router({mode: 'history', routes})
---
+++ src/views/PageAbout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="column is-offset-1">
+    <h1 class="title">This is About page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+</style>
