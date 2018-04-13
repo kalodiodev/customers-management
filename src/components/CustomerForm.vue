f28055b3b1c66258111ba451f29216ae25839d6a
Athanasios Raptodimos <thanosra@gmail.com>
2018-04-13 07:16:14 +0300

    Export to customer form to component

---
+++ src/components/CustomerForm.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <label class="label">First Name</label>
+          <p class="control">
+            <input class="input" type="text" placeholder="Type First Name">
+          </p>
+        </div>
+        <div class="field">
+          <label class="label">Last Name</label>
+          <p class="control">
+            <input class="input" type="email" placeholder="Type Last Name">
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <label class="label">Email</label>
+          <p class="control">
+            <input class="input" type="text" placeholder="Email">
+          </p>
+        </div>
+        <div class="field">
+          <label class="label">Phone</label>
+          <p class="control">
+            <input class="input" type="text" placeholder="Phone">
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <label class="label">Address</label>
+          <p class="control">
+            <input class="input" type="text" placeholder="Address">
+          </p>
+        </div>
+        <div class="field">
+          <label class="label">City</label>
+          <p class="control">
+            <input class="input" type="text" placeholder="City">
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Country</label>
+      <p class="control">
+        <input class="input" type="text" placeholder="City">
+      </p>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-body">
+        <div class="field">
+          <label class="label">Tax ID</label>
+          <p class="control">
+            <input class="input" type="text" placeholder="Tax ID">
+          </p>
+        </div>
+        <div class="field">
+          <label class="label">Profession</label>
+          <p class="control">
+            <input class="input" type="text" placeholder="Profession">
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Notes</label>
+      <p class="control">
+        <textarea class="input" placeholder="Notes"></textarea>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
