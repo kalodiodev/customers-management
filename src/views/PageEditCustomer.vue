b077b8220ba1a9599f415f73239a94eec5c2f43e
Athanasios Raptodimos <thanosra@gmail.com>
2018-04-14 21:17:14 +0300

    Add update customer method

--- src/views/PageEditCustomer.vue
+++ src/views/PageEditCustomer.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section">
     <h2 class="title is-3">Edit Customer</h2>
-    <customer-form></customer-form>
+    <customer-form @saveCustomer="updateCustomer"></customer-form>
   </section>
 </template>
 
@@ -11,6 +11,16 @@ import CustomerForm from '@/components/CustomerForm'
 export default {
   components: {
     CustomerForm
+  },
+
+  created: {
+
+  },
+
+  methods: {
+    updateCustomer (customer) {
+      // TODO: Save updated customer
+    }
   }
 }
 </script>
